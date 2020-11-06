1bc84636b240b98c7b792b98f1d138a63dcf558f
alvarofelipe12 <alvarofelipe12@gmail.com>
2020-11-06 13:24:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-eunpxf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-eunpxf)
---
+++ src/index2.js
@@ -0,0 +1,8 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import App from "./App";
+// import Game from "./Game";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<Game />, document.getElementById("root"));
